1d729fdcf22e560f897e369f160a7ebdd8db0a97
Trevor Chinn <revolution.x@gmail.com>
2021-07-04 23:18:52 +0900

    Fixed merge conflicts

--- main.js
+++ main.js
@@ -24,11 +24,7 @@ function createMainWindow() {
         slashes: true
     }));
 
-<<<<<<< HEAD
     // mainWindow.openDevTools();
-=======
-    mainWindow.openDevTools();
->>>>>>> 0c0a2033ef243418b50b18611921f419347c3a1f
 }
 
 function showSettingsWindow() {
